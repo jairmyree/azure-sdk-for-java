a662350c2e6d4c2a2d060c28452b032b7d9ddb2b
jairmyree <jairmyree@microsoft.com>
2025-01-02 13:19:11 -0800

    Test Changes

--- eng/versioning/dependabot-update-external-dependencies.py
+++ eng/versioning/dependabot-update-external-dependencies.py
@@ -3,8 +3,13 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description="Print out the names of dependencies.")
     parser.add_argument('--dependencies', "-d", metavar='D', type=str, nargs='+', help='a list of dependencies')
+    parser.add_argument("--variable", type=str)
+    parser.add_argument("--json", type=str)
     args = parser.parse_args()
 
+
+    print(args.variable)
+    print(args.json)
     for dependency in args.dependencies:
         print(dependency)
 
